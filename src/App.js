d2863eca90b093b30857f2dc52bbd31bea67f89d
York-Citynew <y.c.new.a@gmail.com>
2022-11-22 18:26:27 +0330

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Auth from "./routes/auth/auth.component";
 import Checkout from "./routes/checkout/checkout.component";
+import { onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+import { USER_ACTION_TYPES } from "./store/user/user.action.types";
+
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route
