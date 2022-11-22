f7e5d0171d4104d03ced63064da606e8312c524f
York-Citynew <y.c.new.a@gmail.com>
2022-11-22 23:41:34 +0330

    fixing node modules

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import Navigation from "./routes/navigation/navigation.component";
 import Auth from "./routes/auth/auth.component";
 import Checkout from "./routes/checkout/checkout.component";
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    });
+    return unsubscribe;
+  }, [dispatch]); //just to get rid of "missing dependency" error
+
   return (
     <Routes>
       <Route
