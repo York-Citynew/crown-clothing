6d7245f68cedbd22cb2f8b84ab96c8863085a988
York-Citynew <y.c.new.a@gmail.com>
2022-12-11 14:21:32 +0330

    added PWA feature

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import { GlobalStyles } from "./index.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./store/store";
 import { Elements } from "@stripe/react-stripe-js";
 import { stripePromise } from "./utils/stripe/stripe.utils";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -20,6 +21,7 @@ root.render(
       >
         <BrowserRouter>
           <Elements stripe={stripePromise}>
+            <GlobalStyles />
             <App />
           </Elements>
         </BrowserRouter>
@@ -32,3 +34,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
